8abff7067d2c5017b1db4252f5b1fcd67be37fb0
suhaohao <suhaohao@enn.cn>
2019-05-16 10:04:42 +0800

    Initial commit

---
+++ gaea-gateway/src/main/java/org/greece/mythology/gaea/GaeaApplication.java
@@ -0,0 +1,12 @@
+package org.greece.mythology.gaea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaeaApplication.class, args);
+    }
+}
