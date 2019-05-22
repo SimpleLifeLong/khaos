172fa0e9eab33e2ecaa2366625a6efabdcddba1a
suhaohao <suhaohao@enn.cn>
2019-05-22 17:15:00 +0800

    add data jpa

--- uranus-interface-normal/src/main/java/org/greece/mythology/uranus/repository/PreventionAlarmRuleRepository.java
+++ uranus-interface-normal/src/main/java/org/greece/mythology/uranus/repository/PreventionAlarmRuleRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PreventionAlarmRuleRepository extends JpaRepository<PreventionAlarmRule, Long> {
 
-    @Query(value = "select * from PreventionAlarmRule where id = ?1", nativeQuery = true)
+    @Query(value = "select * from prevention_alarm_rule where id = ?1", nativeQuery = true)
     public PreventionAlarmRule getByIdAndStationIdLike(Long id);
 }
